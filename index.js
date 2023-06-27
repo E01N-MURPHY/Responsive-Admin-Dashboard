958f3258e9f302750becb70c6f81f79acaa7903f
E01N MURPHY <eoinmurphy291@gmail.com>
2023-06-27 06:39:26 +0000

    added comments and theme color change

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const themeToggle = document.querySelector(".theme-toggle");
 
+//show sidebar
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
 })
 
+//close sidebar
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
+})
+
+//change color theme
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggle.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggle.querySelector('span:nth-child(2)').classList.toggle('active');
 })
